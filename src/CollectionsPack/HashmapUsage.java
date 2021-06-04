98d386d35f09382911a64ee438992ab9290a3496
rahul <rahuldhiman4@gmail.com>
2021-06-04 14:01:04 +0530

    HashMapUsage and filtering, sorting

--- src/CollectionsPack/HashmapUsage.java
+++ src/CollectionsPack/HashmapUsage.java
@@ -1,18 +1,27 @@
 package CollectionsPack;
-
+/*
+1. The hierarchy of Java Map is given below:
+Map (Interface) <------SortedMap (Interface)<------TreeMap (Class)
+Map (Interface) <------Hashmap (Class) <--------- LinkedHashMap (Class)
+2. A Map is useful if you have to search, update or delete elements on the basis of a key.
+3. A Map doesn't allow duplicate keys, but you can have duplicate values. HashMap and LinkedHashMap allow null keys and
+values (but only 1 null key), but TreeMap doesn't allow any null key.
+4. A Map can't be traversed, so you need to convert it into Set using keySet() or entrySet() method.
+5. HashMap is the implementation of Map, but it doesn't maintain any order.
+   LinkedHashMap is the implementation of Map. It inherits HashMap class. It maintains insertion order.
+   TreeMap is the implementation of Map and SortedMap. It maintains ascending order.
+ */
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.io.*;
+import java.util.*;
 
 public class HashmapUsage {
     public static void main(String[] args){
         JSONParser parser = new JSONParser();
         HashMap<String,String> hm = new HashMap<>();
+        LinkedHashMap<String,String> lhm = new LinkedHashMap<>();
+        TreeMap<String,String> tm = new TreeMap<>();
         try {
             Object obj = parser.parse(new FileReader("/Users/rahul/Documents/WorkingFolder/data.json"));
             JSONObject jObj = (JSONObject) obj;
@@ -21,14 +30,46 @@ public class HashmapUsage {
                 String key = itrKeys.next();
                 String value = jObj.get(key).toString();
                 hm.put(key,value);
+                lhm.put(key,value);
+                tm.put(key,value);
             }
-            hm.put(null,"Ajay");
-            hm.put(null,"Kanak");
+            hm.put("-1","Kanak");
+            hm.put("98","Raj");
+            hm.put(null,"Ajay"); //doesn't maintain any order.. can print in any order.
 
-            System.out.println(hm);
-        } catch (ParseException | FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            lhm.put("-1","Kanak");
+            lhm.put("98","Raj");
+            lhm.put(null,"Ajay");  //maintains insertion order.. will print elements the way they were inserted.
+
+//            tm.put(null,"Ajay");   //can't put null key
+            tm.put("100",null); //maintains ascending order, will be printed based upon ascending order.
+
+            System.out.println("Hashmap : "+hm);
+            System.out.println("LinkedHashMap "+lhm);
+            System.out.println("TreeMap "+tm);
+
+            /*
+            Three ways to iterate a Hashmap
+                Using a for loop to iterate through a HashMap
+                Using a forEach to iterate through a HashMap
+                Using an iterator to iterate through a HashMap
+             */
+
+            System.out.println("Using a for loop to iterate through a HashMap ");
+            for(Map.Entry m:hm.entrySet()){
+                System.out.println(m.getKey()+" "+m.getValue());
+            }
+
+            System.out.println("Using a forEach to iterate through a HashMap");
+            lhm.forEach((key,value)->System.out.println(key +" = "+value));
+
+            System.out.println("Using an iterator to iterate through a HashMap");
+            Iterator<Map.Entry<String,String>> it = tm.entrySet().iterator();
+            while(it.hasNext()){
+                Map.Entry<String,String> set = it.next();
+                System.out.println(set.getKey() + " = " + set.getValue());
+            }
+        } catch (ParseException | IOException e) {
             e.printStackTrace();
         }
 
--- src/CollectionsPack/StreamsUsage.java
+++ src/CollectionsPack/StreamsUsage.java
@@ -1,27 +1,60 @@
     package CollectionsPack;
 
     import java.util.ArrayList;
-    import java.util.List;
+    import java.util.HashMap;
+    import java.util.*;
     import java.util.stream.Collectors;
 
     public class StreamsUsage {
         public static void main(String[] args) {
             List<Integer> al = new ArrayList<>();
-                al.add(4);
-                al.add(2);
-                al.add(1);
-                al.add(3);
-                al.add(3);
-            List<Integer> al2 = al.stream().filter(x->x>2).collect(Collectors.toList());
-            List<Integer> al3 = al.stream().map(x->x+2).collect(Collectors.toList());
-            List<Integer> al4 = al.stream().sorted((x,y)->x>y?1:x<y?-1:0).collect(Collectors.toList());
+            al.add(4);
+            al.add(2);
+            al.add(1);
+            al.add(3);
+            al.add(3);
+            List<Integer> al2 = al.stream().filter(x -> x > 2).collect(Collectors.toList());
+            List<Integer> al3 = al.stream().map(x -> x + 2).collect(Collectors.toList());
+            List<Integer> al4 = al.stream().sorted((x, y) -> x > y ? 1 : x < y ? -1 : 0).collect(Collectors.toList());
             long cnt = al.stream().distinct().count();
             al2.forEach(System.out::println);
             System.out.println("=============");
             al3.forEach(System.out::println);
             System.out.println("=============");
             al4.forEach(System.out::println);
-            System.out.println("======Distinct count======="+cnt);
+            System.out.println("======Distinct count=======" + cnt);
 
+            Map<Integer, String> mp = new HashMap<Integer, String>();
+            mp.put(3, "Rahul");
+            mp.put(2, "Dhruv");
+            mp.put(8, "Kanak");
+            mp.put(4, "Ekta");
+            mp.put(9, "Puneet");
+            mp.put(5, "Ajay");
+            mp.put(1, "Raj");
+            mp.put(7, "Urmila");
+            mp.put(6, "Heena");
+
+            System.out.println("======Sorting map by key=======");
+            mp.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
+            System.out.println("======Sorting map by key Reverse Order=======");
+            mp.entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())).forEach(System.out::println);
+            System.out.println("======Sorting map by Value=======");
+            mp.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(System.out::println);
+            System.out.println("======Sorting map by Value Reverse Order=======");
+            mp.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEach(System.out::println);
+
+            System.out.println("======Filtering map by key=======");
+            Map<Integer, String> mp2 = mp.entrySet().stream().filter(x -> x.getKey() < 6).collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
+            mp2.forEach((key, value) -> System.out.println(key + " " + value));
+
+            System.out.println("======Filtering map by both key and value=======");
+            Map<Integer, String> mp3 = mp.entrySet().stream().filter(x->x.getKey()<5).filter(x->x.getValue().startsWith("R")).collect(Collectors.toMap(x->x.getKey(),x->x.getValue()));
+            for(Map.Entry m : mp3.entrySet()){
+                System.out.println(m.getKey()+" = "+m.getValue());
+            }
+
+            System.out.println("======Sort above map result by key in reverse.=======");
+            mp3.entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())).forEach(System.out::println);
         }
     }
